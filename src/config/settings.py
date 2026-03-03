8379192a6f5095ac17899ab24c6ddec23038ee40
Khaled <mh.khaled.abas@gmail.com>
2026-03-03 11:40:12 +0300

    - Release 03-03-2026_v2.10.3

--- src/config/settings.py
+++ src/config/settings.py
@@ -43,7 +43,7 @@ class AppConfig:
     Application configuration for ConvuyerBreadBagCounterSystem.
     """
     
-    APP_VERSION: str = "02-03-2026_v2.10.2"
+    APP_VERSION: str = "03-03-2026_v2.10.3"
 
     # Video source for testing
     video_path: str = os.getenv("VIDEO_PATH", "D:\\Recordings\\2026_02_05\\2026_03_02\\output2.mp4")
